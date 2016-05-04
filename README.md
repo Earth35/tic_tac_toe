02e8c86448be0182f0c37c6f83e057c431a16abf
Krzysztof Piotrowski <krzysiekp91@gmail.com>
2016-05-04 21:02:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# tic-tac-toe  
+Tic Tac Toe written in Ruby.  
+Exercise from the Odin Project  
+
+http://www.theodinproject.com/ruby-programming/oop  
---
+++ TicTacToe.rb
@@ -0,0 +1,18 @@
+class TicTacToe
+  # placeholder
+  
+  class Board
+    def initialize
+	  # placeholder
+	end
+  end
+  
+  class Player
+    attr_reader :name
+	def initialize (name, symbol)
+	  @name = name
+	  @symbol = symbol
+	end
+  end
+end
+
